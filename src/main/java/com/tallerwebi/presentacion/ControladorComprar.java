2f4cbe841f2a31afc743e965224834035fbbf02d
LucasHornos <lucashornos04@gmail.com>
2024-06-07 10:04:08 -0300

    mercado pago terminado

--- src/main/java/com/tallerwebi/presentacion/ControladorComprar.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorComprar.java
@@ -1,45 +1,65 @@
 package com.tallerwebi.presentacion;
 
-import com.tallerwebi.dominio.Carrito;
-import com.tallerwebi.dominio.CompraLibroService;
-import com.tallerwebi.dominio.Usuario;
+import com.mercadopago.resources.payment.Payment;
+import com.mercadopago.resources.preference.Preference;
+import com.tallerwebi.dominio.*;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.implementation.bind.annotation.Empty;
 import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
-
 @Controller
 public class ControladorComprar {
 
-
-    private CompraLibroService compraLibroService;
+    private final CompraLibroService compraLibroService;
+    private final MercadoPagoService mercadoPagoService;
 
     @Autowired
-    public ControladorComprar (CompraLibroService compraLibroService) {
+    public ControladorComprar (CompraLibroService compraLibroService,MercadoPagoService mercadoPagoService) {
+        this.mercadoPagoService = mercadoPagoService;
         this.compraLibroService = compraLibroService;
     }
-
-    @GetMapping("/comprar")
+    @PostMapping("/comprar")
     public ModelAndView comprar(HttpServletRequest request) {
         HttpSession sesion = request.getSession();
         Usuario usuario = (Usuario) sesion.getAttribute("USUARIO");
         Carrito carrito = (Carrito) sesion.getAttribute("CARRITO");
-        compraLibroService.registrarCompra(usuario, carrito);
-        carrito.limpiar();
-        sesion.setAttribute("cantidadLibros", 0);
+
+        Preference preference = mercadoPagoService.createPreference(usuario, carrito);
+
+        if (preference != null){
+            compraLibroService.registrarCompra(usuario, carrito);
+
+            carrito.limpiar();
+            sesion.setAttribute("cantidadLibros", 0);
+            return new ModelAndView("redirect:" + preference.getSandboxInitPoint());
+        }
+
         return new ModelAndView("redirect:/home");
     }
 
+    @GetMapping("/payment/feedback")
+    public ModelAndView feedback(@RequestParam("collection_status") String paymentStatus,
+                                 HttpServletRequest request) {
+
+        HttpSession sesion = request.getSession();
+        Usuario usuario = (Usuario) sesion.getAttribute("USUARIO");
+        Carrito carrito = (Carrito) sesion.getAttribute("CARRITO");
+
+        if ("approved".equals(paymentStatus)) {
+            compraLibroService.registrarCompra(usuario, carrito);
+
+            carrito.limpiar();
+            sesion.setAttribute("cantidadLibros", 0);
+            return new ModelAndView("redirect:/home");
+        }
+        return new ModelAndView("redirect:/mostrar-carrito");
+    }
 }

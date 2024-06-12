99f205b150c61c9c5f74cfda24c9390ad094626f
Martu <143520216+MartuPe@users.noreply.github.com>
2024-06-12 11:44:55 -0300

    cambios perfil admin

---
+++ src/main/java/com/tallerwebi/presentacion/ControladorEditarPerfilAdmin.java
@@ -0,0 +1,29 @@
+package com.tallerwebi.presentacion;
+
+import com.tallerwebi.dominio.Usuario;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class ControladorEditarPerfilAdmin {
+
+    @GetMapping("/editar-perfil-admin")
+    public ModelAndView mostrarPerfil(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        Usuario usuario = (Usuario) session.getAttribute("USUARIO");
+
+        if (usuario != null) {
+            ModelMap model = new ModelMap();
+            model.put("usuario", usuario);
+
+            return new ModelAndView("editar-perfil-admin", model);
+        }
+        return new ModelAndView("redirect:/perfilAdmin");
+    }
+
+}

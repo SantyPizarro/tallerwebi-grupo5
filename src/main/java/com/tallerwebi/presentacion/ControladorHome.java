6fbdc9e135b0bc55d043a833040616a62739e75c
Santy <pizarro.santy@gmail.com>
2024-06-15 20:34:23 -0300

    actualizando rama

--- src/main/java/com/tallerwebi/presentacion/ControladorHome.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorHome.java
@@ -1,14 +1,15 @@
 package com.tallerwebi.presentacion;
 
 import com.tallerwebi.dominio.*;
+import com.tallerwebi.dominio.Libro;
+import com.tallerwebi.dominio.SolicitudAmistadService;
+import com.tallerwebi.dominio.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -19,12 +20,14 @@ public class ControladorHome {
     private final ServicioLibro servicioLibro;
     private final ServicioSuscriptor servicioSuscriptor;
     private final ServicioPreferencias servicioPreferencias;
+    private final SolicitudAmistadService solicitudAmistadService;
 
     @Autowired
-    public ControladorHome(ServicioLibro servicioLibro, ServicioSuscriptor servicioSuscriptor, ServicioPreferencias servicioPreferencias) {
+    public ControladorHome(ServicioLibro servicioLibro, ServicioSuscriptor servicioSuscriptor, ServicioPreferencias servicioPreferencias, SolicitudAmistadService solicitudAmistadService) {
         this.servicioLibro = servicioLibro;
         this.servicioSuscriptor = servicioSuscriptor;
         this.servicioPreferencias = servicioPreferencias;
+        this.solicitudAmistadService = solicitudAmistadService;
     }
 
     @GetMapping("/home")
@@ -37,8 +40,11 @@ public class ControladorHome {
             modelAndView.addObject("librosOrdenados", servicioLibro.ordenarPorFechaAgregado());
 
             Preferencias preferencias = new Preferencias(usuario);
+
             List<Libro> librosRecomendados = servicioPreferencias.recomendarLibros(preferencias);
+
             modelAndView.addObject("librosRecomendados", librosRecomendados);
+            modelAndView.addObject("cantidadNotificaciones", solicitudAmistadService.buscarSolicitudes(usuario).size());
             return modelAndView;
         }
 

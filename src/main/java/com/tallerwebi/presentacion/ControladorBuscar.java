75106f0e05687b8e9b008c8013089f80cd3ce04e
Santy <pizarro.santy@gmail.com>
2024-06-18 23:08:50 -0300

    libros-buscar fix
    Ahora muestra los generos correctamente

--- src/main/java/com/tallerwebi/presentacion/ControladorBuscar.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorBuscar.java
@@ -2,6 +2,7 @@ package com.tallerwebi.presentacion;
 
 import com.tallerwebi.dominio.Libro;
 import com.tallerwebi.dominio.ServicioBarraBusqueda;
+import com.tallerwebi.dominio.ServicioLibro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,17 +15,25 @@ import java.util.List;
 public class ControladorBuscar {
 
     private final ServicioBarraBusqueda servicioBarraBusqueda;
+    private final ServicioLibro servicioLibro;
 
     @Autowired
-    public ControladorBuscar(ServicioBarraBusqueda servicioBarraBusqueda) {
+    public ControladorBuscar(ServicioBarraBusqueda servicioBarraBusqueda, ServicioLibro servicioLibro) {
         this.servicioBarraBusqueda = servicioBarraBusqueda;
+        this.servicioLibro = servicioLibro;
     }
 
     @GetMapping("buscar-libro")
     public ModelAndView buscarLibro(@RequestParam("titulo") String titulo) {
         ModelAndView modelAndView = new ModelAndView("libros-buscar");
         List<Libro> librosEncontrados = servicioBarraBusqueda.buscarPorTitulo(titulo);
+        List<String> generos = servicioLibro.obtenerGeneros();
+        List<String> editoriales = servicioLibro.obtenerEditoriales();
+
         modelAndView.addObject("librosEncontrados", librosEncontrados);
+        modelAndView.addObject("generos", generos);
+        modelAndView.addObject("editoriales", editoriales);
+
         return modelAndView;
     }
 }
